5185e780ed10cc951e543b33af9491d7d2897ca9
vtran91 <vtran16@bellsouth.net>
2023-02-27 16:30:03 +0000

    Completed CombineArrays Coding Challenge

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,3 +1,5 @@
+import org.eclipse.jetty.util.ArrayUtil;
+import org.objectweb.asm.tree.IntInsnNode;
 
 public class CombineArrays {
     /**
@@ -8,7 +10,20 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int a1 = arr1.length;
+        int a2 = arr2.length;
 
-        return null;
+        int[] returnArr = new int[a1+a2];
+        for (int i = 0; i < a1; i++)
+        {
+            returnArr[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++)
+        {
+            returnArr[i + a1] = arr2[i];
+        }
+
+        return returnArr;
     }
 }
